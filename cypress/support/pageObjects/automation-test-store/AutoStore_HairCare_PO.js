f63007c9db7648ddc33c7385807403acf812297c
thuyenh1081 <thuyenh1081@gmail.com>
2022-09-12 20:18:30 +0700

    Cypress auto framwork - first commit

---
+++ cypress/support/pageObjects/automation-test-store/AutoStore_HairCare_PO.js
@@ -0,0 +1,12 @@
+class AutoStore_HairCare_Po {
+    addHairCareProductsToBasket() {
+        globalThis.data.productName.forEach(function(element) {
+            // cy.addProductToBasket(element)
+            cy.addProductToBasket(element).then(() => {
+                //debugger
+            })
+        })
+        cy.get('.dropdown-toggle > .fa').click().debug();
+    }
+}
+export default AutoStore_HairCare_Po;
